b204089ef9c295bf176f11b5dd951e1fa0818bc9
DorianKnight <34255884+DorianKnight@users.noreply.github.com>
2022-06-24 17:12:57 -0400

    First draft of correlation coefficient method
    Through using the mathematical technique I proposed on the notion page I was able to code an algorithm that compare the template schedule with the user schedule to determine a correlation coefficient. In this push I've written the first draft to that correlation coefficient through rough observation I seem to get a better score when the schedules align and a worse score when the schedules deviate.

--- ScheduleMe.py
+++ ScheduleMe.py
@@ -40,6 +40,7 @@ class Schedule:
     def __init__(self):
         self.templateSchedule = self.getTemplate()
         self.userSchedule = self.getUser()
+        self.compareList = self.filterUserSchedule()
     def getTemplate(self):
         rawData = self.getEventsList('utg1uq5vc83biaq2p9n9cqq32k@group.calendar.google.com')
         return self.FormatItems(rawData)
@@ -72,12 +73,85 @@ class Schedule:
                 schedule.append([eventTitle, formattedEventStart, eventDuration])
         return schedule
 
+    def filterUserSchedule(self):
+        #Create a list containing all of the event titles within the template list
+        templateEventNames = []
+        filteredUser = []
+        for item in self.templateSchedule:
+            title = item[0]
+            templateEventNames.append(title)
+        
+        #Search for the template event names within the user schedule and append found lists to the filtered user schedule
+        # for item in templateEventNames:
+        #     for listItem in self.userSchedule:
+        #         if item == listItem[0]:
+        #             filteredUser.append(listItem)
+
+        #Create a list where each index contains the template event and its user event counterpart
+        for tItem in self.templateSchedule:
+            for uItem in self.userSchedule:
+                if tItem[0] == uItem[0]:
+                    filteredUser.append([tItem,uItem])
+
+        return filteredUser
+
+    def correlationCoefficient(self):
+        #This function follows the process written on the notion document to determine how similar both schedules are to eachother
+        SSfit = 0.0
+        SSmean = 0.0
+
+        #Treat start time as y and duration as z while the individual discontinuous event represents x
+        for pair in self.compareList:
+            userStart = datetime.datetime.strptime(pair[1][1],"%H:%M")
+            templateStart = datetime.datetime.strptime(pair[0][1],"%H:%M") 
+
+            userDuration = pair[1][2]
+            templateDuration = pair[0][2]
+
+            ydiff = (userStart - templateStart).total_seconds()/3600
+            zdiff = userDuration - templateDuration
+
+            SSfit += pow(ydiff,2) + pow(zdiff,2)
+        
+        #Calculate the mean start time and duration within the template schedule
+        #Then take the difference between the mean and each individual event on the user schedule
+
+        n = len(self.compareList)
+        totalStartTime = 0.0
+        zeroTime = datetime.datetime.strptime('00:00',"%H:%M")
+        totalDuration = 0.0
+        for item in self.compareList:
+            startTime = datetime.datetime.strptime(item[0][1],"%H:%M")
+            timeDelta = (startTime - zeroTime).total_seconds()/3600
+            totalStartTime += timeDelta
+            duration = item[0][2]
+            totalDuration += duration
+
+        averageStartTime = totalStartTime/n #This is the average hour that I have an event at
+        averageDuration = totalDuration/n
+
+        #Calculate the SSmean by finding the distance from each user schedule point to the average line on the template schedule    
+
+        for userVal in self.compareList:
+            userStart = datetime.datetime.strptime(userVal[1][1],"%H:%M")
+            userDuration = userVal[1][2]
+
+            timeDiff = averageStartTime - (userStart - zeroTime).total_seconds()/3600
+            durationDiff = averageDuration - userDuration
+
+            SSmean += pow(timeDiff,2) + pow(durationDiff,2)
+        
+        corellationCoeff = (SSmean - SSfit)/SSmean
+
+        return corellationCoeff
 
 
 
 def main():
     Controller = Schedule()
     print(Controller.userSchedule)
+    print(Controller.compareList)
+    print("\n\n","Your correlation coefficient is",Controller.correlationCoefficient())
 
 if __name__ == '__main__':
     main()
